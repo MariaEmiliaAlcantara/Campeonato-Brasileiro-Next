966742af251a7c9b15dec150740de94583c7e461
MariaSchinoni <maria.schinoni10@gmail.com>
2022-11-23 11:32:27 -0300

    global styles

--- components/globalstyles.tsx
+++ components/globalstyles.tsx
@@ -1,9 +1,8 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   html,
   body {
-    color: ${({ theme }) => theme.colors.primary};
     padding: 0;
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
@@ -18,6 +17,6 @@ const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
   }
-`
+`;
 
-export default GlobalStyle
+export default GlobalStyle;
